99dd1f6f292c701e3398cc098158a4d80312865a
Jonathan <jonathanelian64@gmail.com>
2023-01-13 21:50:02 -0700

    Change nombre to name

--- src/ownership/borrowing.rs
+++ src/ownership/borrowing.rs
@@ -1,28 +1,29 @@
 use std::io::stdin;
 
 pub fn main() {
-    let mut nombre = String::new();
+    let mut name = String::new();
     println!("Ingresa tu nombre");
     stdin().read_line(&mut nombre).expect("Fallo al leer stdin");
-    let len = nombre.len();
-    nombre.truncate(len - 1); // delete \n
+    let len = name.len();
+    name.truncate(len - 1); // delete \n
 
-    if nombre.is_empty() {
+    if name.is_empty() {
         println!("Nombre no provisto usando pepe");
-        nombre = String::from("pepe");
-    } 
-    take_name(&nombre); 
+        name = String::from("pepe");
+    }
+    take_name(&name);
     // el & significa que se presto o se hizo un borrow
     // nombre regresa a este scope y es valido
-    modify_name(&mut nombre);
-    println!("El nombre es: {}", nombre)
+    modify_name(&mut name);
+    println!("El nombre es: {}", name)
 }
 
-fn take_name(name: &String){
+fn take_name(name: &String) {
     println!("el nombre que me prestaron es: {name}");
 }
 
-fn modify_name(name: &mut String){
+fn modify_name(name: &mut String) {
     name.push_str("Dev");
     println!("Nombre modificado: {}", name);
 }
+
